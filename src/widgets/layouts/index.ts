5da7ce547a10baab3b3a7cba9f5bba1011a38173
오수진 <osujin@osujin-ui-MacBookPro.local>
2026-03-02 18:10:11 +0900

    refactor: fix profile dashboard remount

--- src/widgets/layouts/index.ts
+++ src/widgets/layouts/index.ts
@@ -1,3 +1,4 @@
 export { MenuVisibleLayout } from './ui/MenuVisibleLayout'
 export { GlobalLayout } from './ui/GlobalLayout'
 export { ProtectedLayout } from './ui/ProtectedLayout'
+export { ProfilePagesLayout } from './ui/ProfilePagesLayout'
